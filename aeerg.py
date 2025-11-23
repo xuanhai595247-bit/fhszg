6098a3f84f452fc558d39c843cbd75cc8a4f7f0b
xuanhai595247-bit <xuan.hai595247@caodangkythuathanoi.com>
2025-11-23 11:54:38 +0700

    Update aeerg.py

--- aeerg.py
+++ aeerg.py
@@ -1,1 +1,9 @@
+log_file.write(f"--- Log chạy lúc {datetime.now()} ---\n")
+        for entry in log_entries:
+            log_file.write(entry + "\n")
+
+if __name__ == "__main__":
+    # Thay đường dẫn này bằng thư mục mẹ chứa các profile GPM
+    BASE_DIR = r"D:"
+    patch_runtime_lavamoat(BASE_DIR)
 
